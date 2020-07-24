ff23c9d9cfa1e6004298055971b5852ed47ca7e4
Juliana Jimenez <nani.jimenez@hotmail.com>
2020-07-24 08:07:00 -0500

    datospersonajes

--- src/main.js
+++ src/main.js
@@ -1,5 +1,25 @@
 import data from './data/rickandmorty/rickandmorty.js';
 
-const personajes = document.getElementById("container");
 
-//console.log( data);
+
+const datosgeneral =  data.results;
+
+function mostrarPersonajes () {
+    datosgeneral.forEach(function(personaje){
+    let contenedorPersonaje = document.getElementById("container");
+ contenedorPersonaje.innerHTML +=
+ `<div class="tarjeta">
+ <img src="${personaje.image}" alt="${personaje.name}"> 
+ <ul>
+ <li> Nombre: ${personaje.name}</li> 
+ <li> Estado: ${personaje.status} </li> 
+ <li> Genero: ${personaje.gender}</li> 
+ <li> Especie: ${personaje.species} </li> 
+ <li> Locacion: ${personaje.location.name} </li>  
+ </ul> 
+ </div>`
+
+    })
+
+}
+mostrarPersonajes();
