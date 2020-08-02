fb6f06b6eeaaa489a042c0bbd1ad3f38c5470701
Juliana Jimenez <nani.jimenez@hotmail.com>
2020-08-01 22:55:15 -0500

    filter

--- src/data.js
+++ src/data.js
@@ -1,6 +1,14 @@
  import data from './data/rickandmorty/rickandmorty.js';
 
 
+ const Especie = {
+   filtrandoByEspecie :(word) => {
+     let Dato = data.results.filter(Especie => Especie.species.includes(word))
+     return Dato
+ }
+}
+export default Especie;
+
 
 // estas funciones son de ejemplo
 
--- src/main.js
+++ src/main.js
@@ -1,11 +1,35 @@
 import data from './data/rickandmorty/rickandmorty.js';
+import Especie from "./data.js";
+
+const searcher = document.getElementById("buscar especie");
+searcher.addEventListener("click", PersonajesEspecie);
 
 
 
-const datosgeneral =  data.results;
+//Prender y apagar vistas del HTML//
+document.querySelector("#Especie").style.display = 'none';
 
+let cambioP = document.querySelector('#vista2');
+cambioP.addEventListener("click", SelectCap);
+ 
+function SelectCap () {
+    document.querySelector("#container").style.display = 'none';
+    document.querySelector("#Especie").style.display = 'block';
+}
+document.querySelector("#container").style.display = 'block';
+
+let cambioP1 = document.querySelector('#vista1');
+cambioP1.addEventListener("click",returnInit );
+ 
+function returnInit() {
+    document.querySelector("#container").style.display = 'block';
+    document.querySelector("#Especie").style.display = 'none';
+}
+
+//Imprimir datos en pantalla
+const datosGeneral =  data.results.slice(0,30);
 function mostrarPersonajes () {
-    datosgeneral.forEach(function(personaje){
+    datosGeneral.forEach(function(personaje){
     let contenedorPersonaje = document.getElementById("container");
  contenedorPersonaje.innerHTML +=
  `<div class="tarjeta">
@@ -18,8 +42,28 @@ function mostrarPersonajes () {
  <li> Locacion: ${personaje.location.name} </li>  
  </ul> 
  </div>`
-
-    })
-
+})
 }
 mostrarPersonajes();
+
+/*const verMas = document.getElementById("btnmás");
+verMas.addEventListener("click", more):*/
+
+function PersonajesEspecie () {
+    let valueFilter = searcher.value;
+    let datosfilter = Especie.filtrandoByEspecie(valueFilter);
+    datosfilter.forEach(function(personaje){
+    let contenedorPersonaje = document.getElementById("Especie");
+ contenedorPersonaje.innerHTML +=
+ `<div class="tarjeta">
+ <img src="${personaje.image}" alt="${personaje.name}"> 
+ <ul>
+ <li> Nombre: ${personaje.name}</li> 
+ <li> Estado: ${personaje.status} </li> 
+ <li> Genero: ${personaje.gender}</li> 
+ <li> Especie: ${personaje.species} </li> 
+ <li> Locacion: ${personaje.location.name} </li>  
+ </ul> 
+</div>`
+})
+}
