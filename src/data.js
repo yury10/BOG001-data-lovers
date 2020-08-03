dab2c93b783871cf2a9cbe24837e66e8d9e57e8a
Juliana Jimenez <nani.jimenez@hotmail.com>
2020-08-02 19:44:54 -0500

    filtro

--- src/data.js
+++ src/data.js
@@ -9,14 +9,6 @@ export const ordenarPersonajes = (data, option) => {
     }
   });
 
-// estas funciones son de ejemplo
-
-//export const example = () => {
-  //return 'example';
-//};
-
-//export const anotherExample = () => {
-  //return 'OMG';
   if (option == "A-Z") {
     return orderAZ;
   }
--- src/main.js
+++ src/main.js
@@ -11,40 +11,31 @@ const mostrarPersonajes = (datosgeneral) => {
     </div>`;
   });
 };
+let contenedor2=
+document.getElementById("container2")
+const mostrar2 = (datosgeneral) => {
+    datosgeneral.forEach((personaje) => {
+    contenedor2.innerHTML += `<div class= "tarjeta">
+    <img src = "${personaje.image}" alt = "${personaje.name}"</div>
+    <ul>
+    <li> Nombre: ${personaje.name}</li>
+    <li> Estado: ${personaje.status}</li>
+    <li> Genero: ${personaje.gender}</li>
+    <li> Especie: ${personaje.species}</li>
+    </ul>
+    </div>`;
+  });
+};
 
 import data from './data/rickandmorty/rickandmorty.js';
 import Especie from "./data.js";
 import {ordenarPersonajes} from "./data.js";
 
-var valueFilter ="h"
-let searcher = document.querySelector(".select");
-/*searcher.addEventListener("change", (event) => {
-    valueFilter = `${event.target.value}`;
-    console.log(valueFilter);
-    PersonajesEspecie(valueFilter);
-    });*/
-    
-   
-/*const getFilter = (e) => {
-    const btnStatus = e.target.value;
-    PersonajesEspecie(btnStatus);
-    console.log(btnStatus)
-}
-searcher.addEventListener("click", getFilter)*/
-
-console.log(valueFilter)
-
-;
 const selectFilter = document.getElementById("buscar");
 
-
 let btn2 = document.querySelector(".aa")
-/*searcher.addEventListener("change",loadFilter)*/
-/*function loadFilter(event) {
-    
-    PersonajesEspecie( event.target.value);
-}*/
-function PersonajesEspecie () {
+
+/*function PersonajesEspecie () {
     let valueFilter = selectFilter.value;
     let datosfilter = Especie.filtrandoByEspecie(valueFilter);
     console.log("pueba")
@@ -63,11 +54,7 @@ function PersonajesEspecie () {
     </div>`
 })
 }
-
-
-/*btn2.addEventListener('click',pp)*/
-
-
+*/
 const datosgeneral = data.results.slice(0,10);
 
 //Prender y apagar vistas del HTML//
@@ -91,11 +78,7 @@ function returnInit() {
 }
 //Imprimir datos en pantalla
 const datosGeneral =  data.results.slice(0,30);
-/*const verMas = document.getElementById("btnmás");
-verMas.addEventListener("click", more):*/
 
-
-/*PersonajesEspecie(valueFilter)*/
 let contenedorPersonaje = document.getElementById("container");
 let ordenAlfabetico = document.querySelector(".menuOrdenar");
 
@@ -109,27 +92,16 @@ const obtenerOrden = (q) => {
 
 ordenAlfabetico.addEventListener("click", obtenerOrden);
 
-mostrarPersonajes(datosgeneral);
-
-
+function deleted() {
+    contenedor2.innerHTML = "";
+  } 
 selectFilter.addEventListener("change", () => {
   
-        let valueFilter = selectFilter.value;
-        let datosfilter = Especie.filtrandoByEspecie(valueFilter);
-        console.log("pueba")
-        return datosfilter.forEach(function(personaje){
-        let contenedorPersonaje = document.getElementById("Especie");
-        contenedorPersonaje.innerHTML +=
-        `<div class="tarjeta">
-        <img src="${personaje.image}" alt="${personaje.name}"> 
-        <ul>
-        <li> Nombre: ${personaje.name}</li> 
-        <li> Estado: ${personaje.status} </li> 
-        <li> Genero: ${personaje.gender}</li> 
-        <li> Especie: ${personaje.species} </li> 
-        <li> Locacion: ${personaje.location.name} </li>  
-        </ul> 
-        </div>`
-    
-  });
+    let valueFilter = selectFilter.value;
+    let datosfilter = Especie.filtrandoByEspecie(valueFilter);
+    console.log("pueba")
+    console.log(valueFilter)
+    console.log(datosfilter)
+    deleted();
+    mostrar2(datosfilter);
 })  
