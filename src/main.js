af6830e16004cefa5f7b5ff62cdb0eb3f702324d
juliana <nani.jimenez@hotmail.com>
2020-08-04 00:56:46 -0500

    search

--- src/main.js
+++ src/main.js
@@ -1,9 +1,14 @@
-let contenedorPersonaje = document.getElementById("content-personajes");
+import data from './data/rickandmorty/rickandmorty.js';
+import Especie from "./data.js";
+import {ordenarPersonajes} from "./data.js";
+
+let contenedorPersonaje = document.querySelector(".content-personajes")
+
 const mostrarPersonajes = (dato) => {
     dato.forEach((personaje) => {
     contenedorPersonaje.innerHTML += `<div class= "tarjeta">
     <img src = "${personaje.image}" alt = "${personaje.name}"</div>
-    <ul>
+    <ul class ="lista">
     <li> Nombre: ${personaje.name}</li>
     <li> Estado: ${personaje.status}</li>
     <li> Genero: ${personaje.gender}</li>
@@ -13,45 +18,23 @@ const mostrarPersonajes = (dato) => {
   });
 }; 
 
-let contenedor2= document.getElementById("content-personajes")
-const mostrar2 = (datosgeneral) => {
-    datosgeneral.forEach((personaje) => {
-    contenedor2.innerHTML += `<div class= "tarjeta">
-    <img src = "${personaje.image}" alt = "${personaje.name}"</div>
-    <ul>
-    <li> Nombre: ${personaje.name}</li>
-    <li> Estado: ${personaje.status}</li>
-    <li> Genero: ${personaje.gender}</li>
-    <li> Especie: ${personaje.species}</li>
-    </ul>
-    </div>`;
-  });
-};
-
-import data from './data/rickandmorty/rickandmorty.js';
-import Especie from "./data.js";
-import {ordenarPersonajes} from "./data.js";
-
 const selectFilter = document.getElementById("filter-input-species");
 
-const datosgeneral = data.results.slice(0,10);
-mostrarPersonajes(datosgeneral);
+const datosgeneral = data.results;
+mostrarPersonajes(datosgeneral.slice(0,10));
 
-
-let ordenAlfabetico = document.querySelector("filter-input-order");
-
-
+let ordenAlfabetico = document.querySelector(".filter-input-order")
 
 const obtenerOrden = (q) => {
-  const btnOrden = q.target.textContent;
-  console.log(btnOrden);
+  const btnOrden = q.target.value;
+  deleted();
   mostrarPersonajes(ordenarPersonajes(datosgeneral, btnOrden));
 };
 
-/*ordenAlfabetico.addEventListener("click", obtenerOrden);*/
+ordenAlfabetico.addEventListener("click", obtenerOrden);
 
 function deleted() {
-    contenedor2.innerHTML = "";
+    contenedorPersonaje.innerHTML = "";
   } 
 selectFilter.addEventListener("change", () => {
   
@@ -61,5 +44,47 @@ selectFilter.addEventListener("change", () => {
     console.log(valueFilter)
     console.log(datosfilter)
     deleted();
-    mostrar2(datosfilter);
+    mostrarPersonajes(datosfilter);
 })  
+/*menu
+function myFunction() {
+  var x = document.getElementById("navegador");
+  if (x.className === "menuNav") {
+    x.className += " responsive";
+  } else {
+    x.className = "menuNav";
+  }
+}*/
+
+const formulario = document.querySelector("#searchIn");
+const boton = document.querySelector("#boton");
+
+const filtrar =() =>{
+  contenedorPersonaje.innerHTML ="";
+  const texto =formulario.value.toLowerCase();
+  for (let personaje of datosgeneral){
+    let nombre = personaje.name.toLowerCase();
+    console.log(nombre)
+    if (nombre.indexOf(texto) !== -1){
+      contenedorPersonaje.innerHTML += `<div class= "tarjeta">
+    <img src = "${personaje.image}" alt = "${personaje.name}"</div>
+    <ul class ="lista">
+    <li> Nombre: ${personaje.name}</li>
+    <li> Estado: ${personaje.status}</li>
+    <li> Genero: ${personaje.gender}</li>
+    <li> Especie: ${personaje.species}</li>
+    </ul>
+    </div>`;
+
+    }
+  }
+  if (contenedorPersonaje.innerHTML ===''){
+    contenedorPersonaje.innerHTML +=`
+    <ul class ="lista">
+
+    <li> Personaje no encontrado</li>
+    </ul>`
+  } 
+}
+boton.addEventListener("click", filtrar)
+formulario.addEventListener('keyup',filtrar)
